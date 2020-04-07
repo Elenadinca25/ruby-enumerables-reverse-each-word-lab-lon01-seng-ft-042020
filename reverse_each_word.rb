521282ee9c8587cdcf010f96b642ba8c5c6ee909
Elena Dinca <elenadinca25@gmail.com>
2020-04-07 09:29:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,11 @@
 def reverse_each_word(string)
   string.each { |string| string.collect }
 end
+
+
+def reverse_each_word(sentence)
+  reversed = sentence.split(" ").collect do |word|
+  word.reverse
+end
+  reversed.join(" ")
+end
