8ed52e2470efe8b2f6ae84a227e9f34eebedb04d
Elena Dinca <elenadinca25@gmail.com>
2020-04-07 09:50:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -21,8 +21,8 @@ end
 
 
 def reverse_each_word(string)
-array = string.split(" ").collect do |string|
-  
+array = string.split(" ")
+
 new_array = []
 array.each do |string|
   new_array << string.reverse
