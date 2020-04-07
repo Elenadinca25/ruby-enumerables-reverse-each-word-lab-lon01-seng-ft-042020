1e8b95b5a58d42e3813ff00b77a73adf0dc50f51
Elena Dinca <elenadinca25@gmail.com>
2020-04-07 09:40:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,17 @@
 def reverse_each_word(string)
-  string.each { |string| string.collect }
+array = string.split(" ")
+my_array = []
+array.collect do |string|
+  my_array.push(string.reverse)
+end
+my_array.join(" ")
 end
 
-
+=begin
 def reverse_each_word(sentence)
   reversed = sentence.split(" ").collect do |word|
   word.reverse
 end
   reversed.join(" ")
 end
+=end
