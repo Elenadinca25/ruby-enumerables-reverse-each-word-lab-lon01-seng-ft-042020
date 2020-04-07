f83eb725bee4854745565f7891cc871951746a2e
Elena Dinca <elenadinca25@gmail.com>
2020-04-07 09:59:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -18,19 +18,19 @@ end
 =end
 
 def reverse_each_word_with_each(string)
-  original_array = string.split(" ")
- return_array = []
- original_array.each do|string|
-   return_array << string.reverse
+  array = string.split(" ")
+  my_array = []
+  array.each do|string|
+   my_array << string.reverse
  end
- return_array.join(" ")
+ my_array.join(" ")
 end
 
 def reverse_each_word(string)
   array = string.split(" ")
-  test_array = []
+  my_array = []
   array.collect do|string|
-    test_array << string.reverse
+    my_array << string.reverse
   end
-test_array.join(" ")
+my_array.join(" ")
 end
