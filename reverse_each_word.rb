3f23e94e4d0eaf8ca481b8546cd52e16d34278c6
Elena Dinca <elenadinca25@gmail.com>
2020-04-07 10:02:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,29 +1,10 @@
-=begin
-def reverse_each_word(string)
-array = string.split(" ")
-my_array = []
-array.collect do |string|
-  my_array.push(string.reverse)
-end
-my_array.join(" ")
-end
-
-
-def reverse_each_word(string)
-  reversed = string.split(" ").collect do |word|
-    word.reverse
-  end
-  reversed.join(" ")
-end
-=end
-
 def reverse_each_word_with_each(string)
   array = string.split(" ")
   my_array = []
   array.each do|string|
    my_array << string.reverse
- end
- my_array.join(" ")
+   end
+  my_array.join(" ")
 end
 
 def reverse_each_word(string)
@@ -31,6 +12,6 @@ def reverse_each_word(string)
   my_array = []
   array.collect do|string|
     my_array << string.reverse
-  end
-my_array.join(" ")
+    end
+  my_array.join(" ")
 end
